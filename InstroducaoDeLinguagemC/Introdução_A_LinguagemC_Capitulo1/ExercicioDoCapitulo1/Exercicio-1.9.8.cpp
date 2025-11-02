dc3146d38b5a255d1c31fc61682b3dbf7cef85b0
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2025-11-02 12:52:16 -0300

    Update Exercicio-1.9.8.cpp

--- InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.8.cpp
+++ InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.8.cpp
@@ -15,22 +15,22 @@
 
 int main(){
 
-    double a,x,resultado;
-
-    printf("Digite um numero(A): ");
-    scanf("%lf",&a);
-    printf("Digite um numero(x): ");
-    scanf("%lf",&x);
-
+    double x1 = 10e-12, x2 = 10e-13, x3 = 10e-14, x4 = 10e-15, x5 = 10e-16;
     //A) (1+x)/x, x = 10e-12, x = 10e-13, x = 10e-14, x= 10e-15, x = 10e-16,...
-    resultado = (1+x) -1 /x;
+     double resultado = (1+x1) -1 /x1;
+     double resultado1 = (1+x2) -1 /x1;
+     double resultado2 = (1+x3) -1 /x1;
+     double resultado3 = (1+x4) -1 /x1;
+     double resultado4 = (1+x5) -1 /x1;
 
+
+     float x8 = 10e12, x9 = 10e13, x10 = 10e13, x11 = 10e14, x12 = 10e15, x13 = 10e16;
     //B) (1+1/x)ˆx , x = 10e12, x = 10e13, x = 10e14, x = 10e15, x = 10e16, ...
-    double resultado1 = powf((1 + 1/x),x);
-
-    printf(" %lf ",resultado);
-    printf("\n");
-    printf(" %lf ",resultado1);
+//    double resultado1 = powf((1 + 1/x),x);
+//
+//    printf(" %lf ",resultado);
+//    printf("\n");
+//    printf(" %lf ",resultado1);
 
     return 0;
 }
