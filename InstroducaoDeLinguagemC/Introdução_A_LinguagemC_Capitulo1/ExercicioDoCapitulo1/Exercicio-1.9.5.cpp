0933262d9cff4a28e5752f4c4c7c48cbd2c7a819
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2025-11-02 10:45:24 -0300

    Update Exercicio-1.9.5.cpp

--- InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.5.cpp
+++ InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.5.cpp
@@ -11,6 +11,20 @@ void Programa1(){
     printf("a = %f , b = %f, resultado = %f",a,b,resultado);
 }
 
+void Programa2(){
+    double resultado = 1/2;
+    printf("O resultado de dois numero com ponto flutuante eh: %lf",resultado);
+}
+
+void Programa3(){
+    double resultado = 1.0/ 2.0;
+    printf("O resultado de dois numero com ponto flutuante eh: %lf",resultado);
+}
+
 int main(){
     Programa1();
+    printf("\n");
+    Programa2();
+    printf("\n");
+    Programa3();
 }
