7da058f54f3b0612159dbac62c525c5140727fec
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2025-11-02 11:31:28 -0300

    Update Exercicio-1.9.7.cpp

--- InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.7.cpp
+++ InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.7.cpp
@@ -1,5 +1,14 @@
-#include <cstdio>
+//
+// Created by Robert L Resende on 02/11/25.
+//
+/*Exercicio 1.9.7 Escreva um programa que lê dois números inteiros
+ * a e b imprime o resultado do teste a > b*/
+#include "cstdio"
 
 int main(){
-  
+
+    int a,b;
+    printf("Digite um numero a e o numero b: ");
+    scanf("%d %d",&a,&b);
+    printf("O numero a eh maior que b ou o numero a e menor que: %d",a>b);
 }
