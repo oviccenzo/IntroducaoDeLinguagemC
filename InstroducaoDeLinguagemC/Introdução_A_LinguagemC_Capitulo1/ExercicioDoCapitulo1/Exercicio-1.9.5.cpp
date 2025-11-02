0c37dfa63f5dd2e5fda1246951126fa9e1bddab6
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2025-11-02 10:16:11 -0300

    Update Exercicio-1.9.5.cpp

--- InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.5.cpp
+++ InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.5.cpp
@@ -3,6 +3,14 @@
 //
 #include "cstdio"
 
+void Programa1(){
+    double a,b,resultado;
+    a = 1;
+    b = 2;
+    resultado = a/b;
+    printf("a = %f , b = %f, resultado = %f",a,b,resultado);
+}
+
 int main(){
-    
+    Programa1();
 }
