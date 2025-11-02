b5d7bfd7f33fa4f40d20ffa044d2ffd5bb5d75a7
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2025-11-02 11:13:04 -0300

    Update Exercicio-1.9.10.cpp

--- InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.10.cpp
+++ InstroducaoDeLinguagemC/Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.10.cpp
@@ -1,1 +1,13 @@
+//
+// Created by Robert L Resende on 02/11/25.
+//
+#include "cstdio"
+#include "cmath"
 
+int main(){
+
+    double x = 1;
+    double a = 1 - cos(x);
+    double b = 1/2*pow((sin(x/2)),2);
+
+}
