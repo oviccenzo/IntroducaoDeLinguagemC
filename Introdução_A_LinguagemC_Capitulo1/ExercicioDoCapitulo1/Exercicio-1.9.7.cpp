b42cc713d8d38b0d6a78c675edefe5a619caa035
viccenzo <viccenzo.oliveira@gmail.com>
2025-11-02 11:30:48 -0300

    initial commit

--- Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.7.cpp
+++ Introdução_A_LinguagemC_Capitulo1/ExercicioDoCapitulo1/Exercicio-1.9.7.cpp
@@ -1,8 +1,14 @@
 //
 // Created by Robert L Resende on 02/11/25.
 //
+/*Exercicio 1.9.7 Escreva um programa que lê dois números inteiros
+ * a e b imprime o resultado do teste a > b*/
 #include "cstdio"
 
 int main(){
 
+    int a,b;
+    printf("Digite um numero a e o numero b: ");
+    scanf("%d %d",&a,&b);
+    printf("O numero 1 eh verdadeiro ou 0 eh falso: %d",a>b);
 }
